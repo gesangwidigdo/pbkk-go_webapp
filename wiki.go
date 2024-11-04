44d2dd5e2585625c63d93a9be81f23331cef83d3
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-11-04 08:10:46 +0700

    feat: saveHandler

--- wiki.go
+++ wiki.go
@@ -56,6 +56,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body") // based on form name
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	// p1.save()
@@ -64,7 +72,7 @@ func main() {
 	// println(string(p2.Body))
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	// http.HandleFunc("/save/", viewHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
